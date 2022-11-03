d4484a4d3301cdb6ae8f18135480ecc6870d19a2
Lukman143 <sklukman98@gmail.com>
2022-11-03 14:50:07 +0530

    update

--- src/com/sk/linkedlist/Main.java
+++ src/com/sk/linkedlist/Main.java
@@ -5,18 +5,22 @@ import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-
-		LL list = new LL();
+		/*
+		 * LL list = new LL();
+		 * 
+		 * list.insertFirst(11); list.insertFirst(12); list.insertFirst(13);
+		 * list.insertLast(14); list.insertLast(15); list.insert(100, 3); //
+		 * System.out.println(list.deleteLast()); //
+		 * System.out.println(list.deleteFirst()); System.out.println(list.delete(2));
+		 * list.display();
+		 */
+		
+		
+		DLL list = new DLL();
 
 		list.insertFirst(11);
 		list.insertFirst(12);
 		list.insertFirst(13);
-		list.insertLast(14);
-		list.insertLast(15);
-		list.insert(100, 3);
-		// System.out.println(list.deleteLast());
-		// System.out.println(list.deleteFirst());
-		System.out.println(list.delete(2));
 		list.display();
 
 	}
